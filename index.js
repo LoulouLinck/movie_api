8294db40ca2472815b5d3966a899b013e2b56449
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-08-21 16:36:26 +0200

    created get requests

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+    {
+      title: 'Revenge',
+      author: 'Yermek Shinarbayev'
+    },
+    {
+      title: 'Three Sisters',
+      author: 'Wang Bing'
+    },
+    {
+      title: 'In the Mood for Love',
+      author: 'Wong Kar Wai'
+    },
+    {
+      title: 'The Mirror',
+        author: 'Andrei Tarkovsky'
+    },
+    {
+      title: '',
+      author: ' '
+    },
+    {
+      title: '',
+      author: ' '
+    },
+    {
+      title: '',
+      author: ' '
+    },
+    {
+      title: '',
+      author: ' '
+    },
+    {
+      title: '',
+      author: ' '
+    },
+    {
+      title: '',
+      author: ' '
+    },
+  ];
+  
+  // GET requests
+    app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+  
+  app.get('/', (req, res) => {
+    res.send('Welcome to my movie club!');
+  });
+  
+//   app.get('/documentation', (req, res) => {                  
+//     res.sendFile('public/documentation.html', { root: __dirname });
+//   });
+  
+
+  // listen for requests
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
