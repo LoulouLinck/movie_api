0c9823b2eeed49ea567374edeb8f602e58a64b6b
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-09-12 22:39:48 +0200

    added bcrypt
    to hash and compare submitted data to hashed data in DB

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); //hashes users’ passed data
 
 let movieSchema = mongoose.Schema({
     Title: { type: String, required: true },
@@ -25,6 +26,14 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => { //hashes submitted passwords
+    return bcrypt.hashSync(password, 10);
+  };
+  
+  userSchema.methods.validatePassword = function(password) { //compares submitted hashed passwords w/ that stored in our DB.
+    return bcrypt.compareSync(password, this.Password);
+  };
 
 let Movie = mongoose.model('Movie', movieSchema);// creates model + collection 'db.movies' w/in MongoDB database: myFlixDB
 let User = mongoose.model('User', userSchema); //creates model + collection 'db.users' w/in MongoDB database: myFlixDB
