637e61e809debf4313d6faa5b120a89455176e0e
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-09-12 22:41:32 +0200

    restricts API to allowed origins

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express'),
    uuid = require('uuid');
    mongoose = require('mongoose');
    Models = require('./models.js'); //require models defined models.js file
-
+const { check, validationResult } = require('express-validator');
 const app = express();
 const Movies = Models.Movie; //ref to model names in model.js
 const Users = Models.User;
@@ -19,8 +19,25 @@ app.use(express.static('public')); // shortcut to avoid multiple res.send() for 
 app.use(express.json()); // import body-parser when Express above v4.16
 app.use(express.urlencoded({ extended: true }));
 
+const cors = require('cors');
+// app.use(cors()); // precises app uses CORS: allows req. from all origins by default
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com']; // sets list of allowed origins
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
 let auth = require('./auth')(app); // Import our “auth.js” file into our project. Argument (app)passed to ensure Express available in “auth.js" 
 const passport = require('passport'); require('./passport');
+
 
   // GET/READ requests
   //Gets default text as a response to '/'
@@ -142,20 +159,35 @@ async(req, res) => {
 
 // POST/CREATE REQUESTS w/ Mongoose promise function
 // Creates new user 
-app.post('/users', async (req, res) => {
-  await Users.findOne({ Username: req.body.Username})
+app.post('/users', 
+// Validation logic for request
+[
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], async (req, res) => {
+
+// check the validation object for errors
+let errors = validationResult(req);
+
+if (!errors.isEmpty()) {
+  return res.status(422).json({ errors: errors.array() });
+}
+
+  let hashedPassword = Users.hashPassword(req.body.Password); // hashes password before storing it MongoDB
+  await Users.findOne({ Username: req.body.Username}) // Search to see if a user with the requested username already exists
      .then ((user) => {
-        if (user) {
+        if (user) { //If the user is found, send a response that it already exists
          return res.status(400).send(req.body.Username + 'already exists');
-       } else {
+       } else { 
         Users.create({
            Username: req.body.Username,
-           Password: req.body.Password,
+           Password: hashedPassword, // stores hashed password
            Email: req.body.Email,
            Birthday: req.body.Birthday
           })
-           .then((user) => {res.status(201).json(user);
-          })
+           .then((user) => { res.status(201).json(user) })
            .catch((error) => {
            console.error(error);
            res.status(500).send('Error: ' + error);
@@ -268,4 +300,8 @@ app.use((err, req, res, next) => {
   });
 
   // listen for requests
-  app.listen(8080, (req, res) => console.log('Your app is listening on port 8080.'));
+  // app.listen(8080, (req, res) => console.log('Your app is listening on port 8080.')); // when locally hosted
+  const port = process.env.PORT || 8080;
+  app.listen(port, '0.0.0.0',() => {
+   console.log('Listening on Port ' + port);
+  });
