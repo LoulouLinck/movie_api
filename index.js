66627699d4b34bdb7933d69bf908e389c2162dcf
Laure Lincker <136842839+LoulouLinck@users.noreply.github.com>
2023-11-29 15:25:27 +0100

    added user endpoint

--- index.js
+++ index.js
@@ -108,6 +108,18 @@ require('./passport');
     res.send('Welcome to my movie club!');
   });  
 
+  // Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ username: req.params.username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
 /**
  * Gets all the movies
  * @name getMovies
